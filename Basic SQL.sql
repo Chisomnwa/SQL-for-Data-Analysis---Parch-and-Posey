55d542484286deb38d387f0ec6ec5b2288dad1b7
Chisomnwa <nnamanichisomnpc@gmail.com>
2022-12-15 11:53:03 +0100

    first commit

--- Basic SQL.sql
+++ Basic SQL.sql
@@ -40,6 +40,7 @@ FROM web_events;
 
 
 
+
 /* USING TOP statement
 NOTE: Not all database systems support SELECT TOP statements. MySQL and Postgre support LIMIT clause
 to select a minimum number of records while Oracle uses FETCH FIRST n ROWS ONLY and ROWNUM*/
@@ -52,6 +53,7 @@ FROM web_events;
 
 
 
+
 /*USING DISTINCT*/
 
 /*Write a query to return the distinct channels in the web_events table*/
@@ -59,6 +61,7 @@ FROM web_events;
 SELECT DISTINCT channel
 FROM web_events;
 -- There are 6 distinct channels on the web events table
+
 
 
 
@@ -102,6 +105,7 @@ ORDER BY total_amt_usd DESC, account_id;
 
 
 
+
 /*USING the WHERE clause*/
 
 /*Write a query that returns the first 5 rows and all columns from the orders 
@@ -128,7 +132,8 @@ WHERE name = 'EOG Resources';
 
 
 
-/*USING ARITHMETIC OPEARTIONS*/
+
+/*USING ARITHMETIC OPERATORS*/
 
 /* Create a column that divides the gloss_amount_usd by the gloss_quantity to find the unit price for the standard paper
 paper for each order. Limit the results to the first 10 orders, and include the id and the account_id field.*/
@@ -149,23 +154,27 @@ FROM orders;
 
 
 
-/*USING THE LIKE operator*/
 
--- Write a query that returns all the companies whose name starts with 'C'
+/*USING the LIKE operator*/
+
+/*Write a query that returns all the companies whose name starts with 'C'.*/
+
 SELECT name
 FROM accounts
 WHERE name LIKE 'C%';
 -- 37 companies have their names starting with C
 
 
--- Write a query that returns all companies whose names contain the string 'one' somewhere in the name
+/*Write a query that returns all companies whose names contain the string 'one' somewhere in the name.*/
+
 SELECT name
 FROM accounts
 WHERE name LIKE '%one%';
 -- 6 companies have the string 'one' somewhere in their names 
 
 
--- Write a query that returns all companies whose names end with 's'
+/*Write a query that returns all companies whose names end with 's'.*/
+
 SELECT name
 FROM accounts
 WHERE name LIKE '%s';
@@ -173,6 +182,118 @@ WHERE name LIKE '%s';
 
 
 
+
+/*USING THE IN operator*/
+
+/*Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.*/
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom')
+
+
+/*Use web-events table to find all information regarding all individuals who were contacted via channel of organic or adwords.*/
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+-- 1,858 inviduals werecontacted via organic or adwords
+
+
+
+
+/*USING NOT operator*/
+
+/*Use web-events table to find all information regarding all individuals who were
+contacted via any method except using organic or adwords methods.*/
+
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords')
+-- 7,215 inviduals were contacted via other methods except organic or adwords
+
+
+/*Use the accounts table to find:
+i. all the companies whose name do not start with 'c'.*/
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%';
+-- 314 companies have their names not starting with C
+
+
+/*ii. all the companies whose names do not contain the string 'one'.*/
+SELECT name
+FROM accounts
+WHERE name NOT LIKE '%one%';
+-- 346 companies do not have the string 'one' in their names
+
+
+
+
+/*USING AND and BETWEEN operators*/
+
+/*Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.*/
+
+SELECT  *
+FROM orders 
+WHERE standard_qty > 1000
+	AND poster_qty = 0
+	AND gloss_qty = 0;
+
+
+/*Using the accounts table, find all the companies whose names do not start with 'c' and end with 's'.*/
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%'
+	AND name LIKE '%s';
+
+
+/*Write a query that displays the order date and gloss_qty data for all orders where gloss_qty data is between 24 and 29.*/
+
+SELECT occurred_at, gloss_qty
+FROM orders 
+WHERE gloss_qty BETWEEN 24 AND 29;
+
+
+/*Use the web_events table to find all the information regarding all individuals who were contacted via the organic
+or adwords channels, and started their account at any point in 2016, sorted from newest to oldest.*/
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+
+
+
+/*USING the OR operator*/
+
+/*Find the list of order_ids where either gloss_qty or poster_qty is greater than 4000. 
+Only include the id field in the resulting table.*/
+
+SELECT id, gloss_qty, poster_qty
+FROM orders
+WHERE gloss_qty > 4000 
+	OR poster_qty > 4000;
+
+
+/*Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qtyis over 1000.*/
+
+SELECT id, standard_qty, gloss_qty, poster_qty
+FROM orders
+WHERE standard_qty = 0
+	AND (gloss_qty > 1000
+	OR poster_qty > 1000);
+
+
+/*Find all company names that start with a 'C' or 'W', and the primary contact contains 'ana, or 'Ana', but it doesn't contain 'eana'.*/
+SELECT name, primary_poc
+FROM accounts 
+WHERE (name LIKE 'C%' OR name LIKE 'W%')
+	AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
+	AND (primary_poc NOT LIKE '%eana%');
 
 
 
